952b5ac3e43347698b311e58b375547309fd5d00
Rage0001 <radiantgraphicsyt@gmail.com>
2019-07-15 16:12:19 +0300

    start code cleanup

--- commands/search.js
+++ commands/search.js
@@ -1,12 +1,8 @@
 const config = require("../config.json");
-const request = require("request");
+const trello = require("../modules/trello.js");
+const request = require('request');
 const Discord = require("discord.js");
-const {
-  promisify
-} = require('util');
 const boards = ["desktop", "ios", "linux", "android", "store", "web", "overlay"]
-
-const requestPromise = promisify(request)
 
 module.exports.run = async (client, message, args) => {
   const labelSeverity = ["red", "orange", "yellow", "green"]
@@ -18,64 +14,14 @@ module.exports.run = async (client, message, args) => {
   if (!boards.includes(board.toLowerCase())) {
     return message.channel.send("Not a valid board.")
   }
-  var boardID
-  switch (board.toLowerCase()) {
-    case "desktop":
-      boardID = config.trello.desktop_bugs
-      break;
-    case "ios":
-      boardID = config.trello.ios_bugs
-      break;
-    case "linux":
-      boardID = config.trello.linux_bugs
-      break;
-    case "android":
-      boardID = config.trello.android_bugs
-      break;
-    case "store":
-      boardID = config.trello.store_bugs
-      break;
-    case "web":
-      boardID = config.trello.website_bugs
-      break;
-    case "overlay":
-      boardID = config.trello.overlay_bugs
-      break;
-    default:
-      boardID = config.trello.desktop_bugs
+  if (!input) {
+    return message.channel.send('Please provide a query.')
   }
+  var boardID = config.trello[board.toLowerCase() + '_bugs']
+
   var resultsEmbed = new Discord.RichEmbed()
 
   let currentPage = 0
-
-  const requestSearch = async (page) => {
-    var options = {
-      method: 'GET',
-      url: 'https://api.trello.com/1/search',
-      qs: {
-        query: input,
-        idBoards: boardID,
-        modelTypes: 'cards',
-        boards_limit: '1',
-        card_fields: 'desc,name,shortUrl,labels,closed',
-        cards_limit: '5',
-        cards_page: String(page),
-        card_list: 'false',
-        card_members: 'false',
-        card_stickers: 'false',
-        card_attachments: 'true',
-        organization_fields: 'name,displayName',
-        organizations_limit: '10',
-        member_fields: 'avatarHash,fullName,initials,username,confirmed',
-        members_limit: '10',
-        partial: 'false',
-        key: process.env.TRELLO_KEY,
-        token: process.env.TRELLO_TOKEN
-      }
-    };
-    const result = JSON.parse((await requestPromise(options)).body)
-    return result.cards
-  }
 
   let botMsg = null
 
@@ -105,7 +51,7 @@ module.exports.run = async (client, message, args) => {
               var ForwardUserReaction = r.message.reactions.filter(r => r._emoji.name === forwardEmoji)
               ForwardUserReaction.first().remove(message.author)
               currentPage++
-              const searchResults = await requestSearch(currentPage)
+              const searchResults = await trello.trelloSearch(input, boardID, currentPage)
               if (searchResults.length === 0) {
                 currentPage--
                 return
@@ -119,7 +65,7 @@ module.exports.run = async (client, message, args) => {
                 return
               }
               currentPage--
-              renderSearch(await requestSearch(currentPage))
+              renderSearch(await trello.trelloSearch(input, boardID, currentPage))
               break;
             default:
               break;
@@ -137,21 +83,9 @@ module.exports.run = async (client, message, args) => {
       if (cards.length === 0) {
         return message.channel.send("No results returned.")
       }
-      var optionstwo = {
-        method: 'GET',
-        url: `https://api.trello.com/1/cards/${cards[0].id}/list`,
-        qs: {
-          fields: 'all',
-          key: process.env.TRELLO_KEY,
-          token: process.env.TRELLO_TOKEN
-        }
-      };
-      request(optionstwo, async function (error, response, bodytwo) {
-        let one = cards[0].desc.replace(/####Steps to reproduce:/g, "➤ __**Steps to reproduce:**__")
-        let two = one.replace(/####Expected result:/g, "➤ __**Expected result:**__")
-        let three = two.replace(/####Actual result:/g, "➤ __**Actual result:**__")
-        let four = three.replace(/####Client settings:/g, "➤ __**Client settings:**__")
-        let final = four.replace(/####System settings:/g, "➤ __**System settings:**__")
+
+        let listName = await trello.getListName(cards[0].id)
+        let formattedDesc = await trello.formatDescription(cards[0].desc)
         var labels = []
         if (cards[0].labels.length !== 0) {
           if (cards[0].labels.length === 1) {
@@ -168,24 +102,31 @@ module.exports.run = async (client, message, args) => {
           }
         }
         let finalLabels = labels.join(" ")
-        await message.channel.send("Here you go, here's what I found:")
+        if (finalLabels === "") finalLabels = "None"
+        message.channel.send("Here you go, here's what I found:")
         if (cards[0].attachments.length !== 0) {
-          await resultsEmbed.setImage(cards[0].attachments[0].url)
+          var youtubeURL = cards[0].attachments[0].url.match('^(https?\:\/\/)?(www\.)?(youtube\.com|youtu\.?be)\/.+$')
+          if (!youtubeURL) {
+            resultsEmbed.setImage(cards[0].attachments[0].url)
+          }
         }
         if (cards[0].name.length > 250) {
-          await resultsEmbed.setTitle(cards[0].name.substring(0, 247) + "...")
+          resultsEmbed.setTitle(cards[0].name.substring(0, 247) + "...")
         } else {
-          await resultsEmbed.setTitle(cards[0].name)
+          resultsEmbed.setTitle(cards[0].name)
         }
-        await resultsEmbed.setDescription(`Labels: ${finalLabels}\nList: ${JSON.parse(bodytwo).name}\nArchived: ${cards[0].closed}\n\n${final}\n\nLink: ${cards[0].shortUrl}`)
-        await resultsEmbed.setColor("#ff3535")
-        await resultsEmbed.setFooter(`Executed by ${message.author.tag}`, message.author.avatarURL)
-        await message.channel.send(resultsEmbed)
-        await client.queries++
-      })
+        if (youtubeURL) {
+          resultsEmbed.setDescription(`Labels: ${finalLabels}\nList: ${listName}\nArchived: ${(cards[0].closed === true) ? "Yes" : "No"}\n\n${formattedDesc}\n\nLink: ${cards[0].shortUrl}\nVideo: ${cards[0].attachments[0].url}`)
+        } else {
+          resultsEmbed.setDescription(`Labels: ${finalLabels}\nList: ${listName}\nArchived: ${(cards[0].closed === true) ? "Yes" : "No"}\n\n${formattedDesc}\n\nLink: ${cards[0].shortUrl}`)
+        }
+        resultsEmbed.setColor("#ff3535")
+        resultsEmbed.setFooter(`Executed by ${message.author.tag}`, message.author.avatarURL)
+        message.channel.send(resultsEmbed)
+        client.queries++
     }
   };
-  renderSearch(await requestSearch(0))
+  renderSearch(await trello.trelloSearch(input, boardID, 0))
 }
 
 module.exports.help = {
