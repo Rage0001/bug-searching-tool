80b428451873dfd7275235d997c6988ec212344d
runic-tears <51153795+runic-tears@users.noreply.github.com>
2019-07-22 21:24:58 +0400

    without comments

--- commands/hug.js
+++ commands/hug.js
@@ -7,19 +7,19 @@ module.exports.run = async (client, message, args) => {
     let target = message.mentions.users.first() || message.guild.members.get(args[0]);
 
     try {
-        if (!target) // checking if the argument was correctly specified
+        if (!target)
             return message.channel.send("I can't find such user :<");
 
-        if (target.id == client.user.id) // Checks if the sender tries to hug me, a little botto :3
+        if (target.id == client.user.id)
             return message.channel.send(`${sender}, aww you're such a cutieee! Thank you for appreciating me **(๑˃̵ᴗ˂̵)ﻭ**`);
-        if (target.id == sender.id) // Checking if the sender tries to hug themselves
+        if (target.id == sender.id)
             return message.channel.send(`Hey, everyone! Look here, **${sender.username}** just tried hugging themselves! Let's give this poor human some love and hug them!!!`);
 
-        let hugRandom = Math.floor((Math.random() * hugMessages.length)); // doing some magic with a list of messages (randomizing)
-        let hugReply = hugMessages[hugRandom].replace('%%target%%', target).replace('%%sender%%', sender.username); // replacing some things from the json to the actual variables, ty Rage <3
-        message.channel.send(hugReply); // hugging a hooman!!!
+        let hugRandom = Math.floor((Math.random() * hugMessages.length));
+        let hugReply = hugMessages[hugRandom].replace('%%target%%', target).replace('%%sender%%', sender.username);
+        message.channel.send(hugReply);
     } catch (e) {
-        message.channel.send(`I actually have no idea how this happened, but here's an error: \n${e}`); // in case if bad stuff happens
+        message.channel.send(`I actually have no idea how this happened, but here's an error: \n${e}`);
     }
 }
 
